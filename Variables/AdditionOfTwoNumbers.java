26ed17c75cc2e5fbde75e574e9135afc83877746
Vikas yadav <vikashyadav06127@gmail.com>
2025-09-10 12:44:01 +0530

    Add files via upload

---
+++ Variables/AdditionOfTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AdditionOfTwoNumbers {
+    public static void main(String[] args) {
+        // Hard-coded numbers
+        double num1 = 5.0;
+        double num2 = 3.0;
+
+        // Perform the addition
+        double sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
